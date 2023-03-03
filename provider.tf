07059e6219215e2edf4eade64365665112972238
Ashok Vishwanth <ashokvishwanth@Ashoks-iMac.local>
2023-03-03 21:45:07 +0530

    code updated

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+    credentials = "251280876200-compute@developer.gserviceaccount.com"
+    project     = "skills-development-375608"
+    region      = "us-west1"
+    zone        = "us-west1-b"
+  }
