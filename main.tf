329c6b0de23a108e9df73ab5e9d9e089ac5843fb
Ashok Vishwanth <ashokvishwanth@Ashoks-iMac.local>
2023-03-03 21:51:09 +0530

    test

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "google_compute_instance" "jenkins-node" {
-  name = "jenkins-node"
+  name         = "jenkins-node"
   machine_type = "f1-micro"
   boot_disk {
-      initialize_params {
-        image = "Ubuntu/Ubuntu 18.04"
-      }
-    }    
-    network_interface {       
-      network = "default"
-      access_config {
-      }
+    initialize_params {
+      image = "Ubuntu/Ubuntu 18.04"
     }
   }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "google" {
-    credentials = "251280876200-compute@developer.gserviceaccount.com"
-    project     = "skills-development-375608"
-    region      = "us-west1"
-    zone        = "us-west1-b"
-  }
+  project     = "skills-development-375608"
+  region      = "us-west1"
+  zone        = "us-west1-b"
+}
